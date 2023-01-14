512ec663f54f7b5bad217e3a15b02080252ac4f1
sherget <sherget0x5d@gmail.com>
2023-01-14 15:12:01 +0100

    Fix init.sh intial Project name

--- init.sh
+++ init.sh
@@ -3,9 +3,10 @@ if [ -z $1 ]; then
   echo 'Please enter a new name for this project';
 else
   echo "Changing Project name to $1...";
-    find . -type f -not -path '*/\.*' -exec sed -i 's/NTP/'"$1"'/g' {} +
+    find . -type f -not -path '*/\.*' -exec sed -i 's/CPPSkeleton/'"$1"'/g' {} +
   echo 'Done!';
 fi
 
 # Build and compile to have the Project ready for development
+sleep 2;
 cd build/ && cmake .. -DCMAKE_BUILD_TYPE=Debug -G "Unix Makefiles" && make all && cd ..
