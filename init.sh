a1ad32e77bc8b7e4b9ec8c6ba3daf89224b6478f
sherget <sherget0x5d@gmail.com>
2023-01-14 15:16:25 +0100

    Change init.sh to be more explicit

--- init.sh
+++ init.sh
@@ -4,9 +4,10 @@ if [ -z $1 ]; then
 else
   echo "Changing Project name to $1...";
     find . -type f -not -path '*/\.*' -exec sed -i 's/CPPSkeleton/'"$1"'/g' {} +
-  echo 'Done!';
 fi
 
 # Build and compile to have the Project ready for development
 sleep 2;
+echo 'Building and compiling your Project...';
 cd build/ && cmake .. -DCMAKE_BUILD_TYPE=Debug -G "Unix Makefiles" && make all && cd ..
+echo 'Done!';
